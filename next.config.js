82730d27389ca7da537e5e18af39fa2511ce50ee
YannickLusci <yan.e.live@live.be>
2023-11-05 20:14:21 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+import {withContentlayer} from "next-contentlayer"
+
 const nextConfig = {}
 
-module.exports = nextConfig
+
+export default withContentlayer({...nextConfig})
